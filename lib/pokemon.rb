16d9b4eb40627dd9a26edbc70daa31b8ab1ecf5f
PepperMcLean <jackmcl206@gmail.com>
2021-05-05 03:46:09 -0500

    commit tree 8c783ac80043a40f9148bbaba7cb87a56de8e2fb

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,29 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(row)
+        @id = row[:id]
+        @name = row[:name]
+        @type = row[:type]
+        @db = row[:db]
+    end
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+        INSERT INTO pokemon (name, type) 
+        VALUES (?, ?)
+        SQL
+
+        db.execute(sql, name, type)
+    end
+
+    def self.find(id, db)
+        sql = <<-SQL
+            SELECT * FROM pokemon WHERE id = ?;
+            LIMIT 1
+        SQL
+        pokemon = db.execute(sql, id).flatten
+        pokemonHash = { id: id, name: pokemon[1], type: pokemon[2], db: db}
+        Pokemon.new(pokemonHash)
+    end
 end
